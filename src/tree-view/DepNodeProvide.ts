565422750dd5cbb3448673011e32714f11f77f0d
maz <margin666@163.com>
2023-06-08 16:25:56 +0800

    首次提交

---
+++ src/tree-view/DepNodeProvide.ts
@@ -0,0 +1,69 @@
+
+import { TreeItemNode } from './TreeItemNode';
+import * as vscode from 'vscode';
+import { Store } from '../index';
+
+export class DepNodeProvide implements vscode.TreeDataProvider<vscode.TreeItem>{
+  stores: Store[];
+  private _onDidChangeTreeData: vscode.EventEmitter<TreeItemNode | undefined | null | void>;
+  readonly onDidChangeTreeData: vscode.Event<TreeItemNode | undefined | null | void>;
+  constructor(store: Store[]) {
+    this.stores = store;
+    this._onDidChangeTreeData = new vscode.EventEmitter<TreeItemNode | undefined | null | void>();
+    this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+  }
+  getTreeItem (element: TreeItemNode): TreeItemNode | Thenable<TreeItemNode> {
+    return element;
+  }
+
+  getChildren (element?: TreeItemNode | undefined): vscode.ProviderResult<TreeItemNode[]> {
+    if (element) {
+      const { type, id, storeId } = element;
+      if (type === 'folder') {
+
+        const store = this.stores.find(v => v.id === storeId);
+        if (!store) {
+          return [];
+        }
+        const treeItemNodes: TreeItemNode[] = [];
+        const folders = store.folders.filter(v => v.parentId === id);
+        folders.forEach(v => {
+          treeItemNodes.push(new TreeItemNode(`🐹${v.label}`, v.id, v.parentId, storeId, 'folder', null, null, 1));
+        });
+        const items = store.state.filter(v => v.folderId === id);
+        items.forEach(v => {
+          treeItemNodes.push(new TreeItemNode(`🌰${v.title}`, v.id, v.folderId, storeId, 'item', v.snippet, v.language, 0));
+        });
+        return treeItemNodes;
+      } else if (type === 'store') {
+        const store = this.stores.find(v => v.id === storeId);
+        if (!store) {
+          return [];
+        }
+        const treeItemNodes: TreeItemNode[] = [];
+        const folders = store.folders.filter(v => v.parentId === id);
+        folders.forEach(v => {
+          treeItemNodes.push(new TreeItemNode(`🐹${v.label}`, v.id, v.parentId, storeId, 'folder', null, null, 1));
+        });
+        const items = store.state.filter(v => v.folderId === id);
+        items.forEach(v => {
+          treeItemNodes.push(new TreeItemNode(`🌰${v.title}`, v.id, v.folderId, storeId, 'item', v.snippet, v.language, 0));
+        });
+        return treeItemNodes;
+
+      } else {
+        return [];
+      }
+    } else {
+      const list = this.stores.map(v => {
+        return new TreeItemNode(`🌳${v.name}`, v.id, '-1', v.id, 'store', null, null, 2);
+      });
+      return list;
+    }
+  }
+
+  refrech () {
+    this._onDidChangeTreeData.fire();
+  }
+
+}
